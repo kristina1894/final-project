a5e2ad528cf30dae8d9888a548232abcecfb832b
Kristina Sfera <sfera.kristina@gmail.com>
2023-06-20 21:48:26 +0300

    creating navigation bar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import Header from './components/Header'
+import  './App.css'
+
 
 function App() {
-
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+        <div className='header'>
+            <div id='logo'>News App</div>
+            <div id='nav-links'>
+                <nav>Home</nav>
+                <nav>Entertainment</nav>
+                <nav>Lifestyle</nav>
+                <nav>Politics</nav>
+                <nav>Sport</nav>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
